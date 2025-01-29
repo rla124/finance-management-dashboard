dbf8a772601d0e5838436fc1ab6e894ce5ce66d1
Subin <rla0010101@gmail.com>
2025-01-29 11:47:24 +0900

    feat: common left side bar && right side bar only for home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,34 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 import React from "react";
 
 const Home = () => {
-  const loggedIn = { firstName: "Subin" };
+  const loggedIn = {
+    firstName: "Subin",
+    lastName: "Kim",
+    email: "kimsubin@developer.com",
+  };
   return (
     <section className="home">
       <div className="home-content">
-        <HeaderBox
-          type="greeting"
-          title="Welcome"
-          user={loggedIn?.firstName || "Guest"}
-          subtext="Access and manage your account and transactions efficiently."
-        />
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
 
-        <TotalBalanceBox
-          accounts={[]}
-          totalBanks={1}
-          totalCurrentBalance={1250.35}
-        />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+        RECENT TRANSACTIONS
       </div>
+      <RightSidebar user={loggedIn} transacions={[]} banks={[{currentBalance: 123.45}, {currentBalance: 678.91}]} />
     </section>
   );
 };
